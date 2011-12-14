b5973967a2300b9eb72ce065c9cf293e353d3ade
robertkrimen <robertkrimen@gmail.com>
2011-12-14 13:58:14 -0800

    Convert to Dist::Zilla and remove cruft

--- lib/Document/TriPart/Cabinet.pm
+++ lib/Document/TriPart/Cabinet.pm
@@ -1,23 +1,8 @@
 package Document::TriPart::Cabinet;
+# ABSTRACT: Document::TriPart::Cabinet 
 
 use warnings;
 use strict;
-
-=head1 NAME
-
-Document::TriPart::Cabinet -
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
-=head1 SYNOPSIS
-
-=cut
 
 use Moose;
 
@@ -63,60 +48,4 @@ sub edit {
     return $document;
 }
 
-=head1 AUTHOR
-
-Robert Krimen, C<< <rkrimen at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-document-tripart-cabinet at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Document-TriPart-Cabinet>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Document::TriPart::Cabinet
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Document-TriPart-Cabinet>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Document-TriPart-Cabinet>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Document-TriPart-Cabinet>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Document-TriPart-Cabinet/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Robert Krimen, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-
-=cut
-
-1; # End of Document::TriPart::Cabinet
+1;
--- t/00-load.t
+++
@@ -1,9 +0,0 @@
-#!perl -T
-
-use Test::More tests => 1;
-
-BEGIN {
-	use_ok( 'Document::TriPart::Cabinet' );
-}
-
-diag( "Testing Document::TriPart::Cabinet $Document::TriPart::Cabinet::VERSION, Perl $], $^X" );
